f2125a0d158644b830ec79aa4f4cdd667217c87a
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2023-12-15 10:32:22 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+nclude "stdio.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int p;
+	for (p = 97; p <= 122; p++)
+	{
+		putchar(p);
+	}
+	putchar('\n');
+	return (0);
+}
