ea3856ff76fcc6d903a1607a656297af15270451
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2023-12-19 09:38:28 -0800

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *  * print_sign - prints the sign of a numbers using function
+ *   *@n: number being tested
+ *    * Return: Always 0 success.
+ *     */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchat(',');
+		_putchat(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchat(',');
+		_putchat(' ');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchat(',');
+		_putchat(' ');
+		return (-1);
+	}
+	return (0);
+}
